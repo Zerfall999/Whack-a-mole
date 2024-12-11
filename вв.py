0d01fd1b20ded8cf2177813bb2153494c3ecb841
arianamihutskaya <158505124+arianamihutskaya@users.noreply.github.com>
2024-12-11 15:41:32 +0300

    Add files via upload

--- вв.py
+++ вв.py
@@ -1,3 +1,4 @@
+
 from tkinter import *
 from PIL import Image
 import random
@@ -19,8 +20,11 @@ highscore = 0
 countdown = 76
 
 app.geometry(f"{width}x{height}")
+# app.attributes('-fullscreen', True)
 app.title("Whack A Squ")
 
+
+# ALL THE FUNCTIONSSSSS
 
 
 # when start button pressed, this will happen, determines the points
@@ -34,8 +38,9 @@ def start():
     playbtn.place_forget()
     playbtn.place_forget()
     kailesurugarne.place(x=570, y=130)
-    t = threading.Thread(target=whack_a_mole)
+    t = threading.Thread(target=whac_a_mole)
     t.start()
+
 
 def onwhack():
     global hole
@@ -60,9 +65,9 @@ def onwhack():
     score += 10
     score_label.configure(text=f"Score:{score}")
 
-# this is the function for the game, this determines where the squirells will spawn, and also when the game end
 
-def whack_a_mole():
+# this is the function for the game, this determines where the rats will spawn, and also when the game end
+def whac_a_mole():
     ready_set_whack()
     county.place(x=540, y=130)
     global countdown
@@ -70,73 +75,105 @@ def whack_a_mole():
     global hole
     global score
     global highscore
-
-    def hit_squirrel(event, button):
-        button.configure(state='disabled')
-        button.place_forget()
-        squirell_sound.play()
-
     for i in range(0, 76):
         countdown -= 1
-        county.configure(text=f"Squirrels Left:{countdown}")
+        county.configure(text=f"Rats Left:{countdown}")
         hole = random.randint(1, 8)
         if hole == 1:
-            trigger_sqrl1.configure(state='normal', image=squirell)
+            squirell_sound.play()
+            trigger_sqrl1.configure(
+                state='normal',
+                image=squirell, )
             trigger_sqrl1.place(x=220, y=255)
-            trigger_sqrl1.bind("<Button-1>", lambda event, button=trigger_sqrl1: hit_squirrel(event, button))
             sleep(0.75)
-            trigger_sqrl1.configure(state='disabled')
+            trigger_sqrl1.configure(state='disabled', )
             trigger_sqrl1.place_forget()
         elif hole == 2:
-            trigger_sqrl2.configure(state='normal', image=squirell)
+            squirell_sound.play()
+            trigger_sqrl2.configure(
+                state='normal',
+                image=squirell, )
             trigger_sqrl2.place(x=620, y=260)
-            trigger_sqrl2.bind("<Button-1>", lambda event, button=trigger_sqrl2: hit_squirrel(event, button))
             sleep(0.75)
-            trigger_sqrl2.configure(state='disabled')
+            trigger_sqrl2.configure(state='disabled', text='', )
             trigger_sqrl2.place_forget()
         elif hole == 3:
-            trigger_sqrl3.configure(state='normal', image=squirell)
+            squirell_sound.play()
+            trigger_sqrl3.configure(
+                state='normal',
+                image=squirell, )
             trigger_sqrl3.place(x=1000, y=255)
-            trigger_sqrl3.bind("<Button-1>", lambda event, button=trigger_sqrl3: hit_squirrel(event, button))
             sleep(0.75)
-            trigger_sqrl3.configure(state='disabled')
+            trigger_sqrl3.configure(state='disabled', text='', )
             trigger_sqrl3.place_forget()
         elif hole == 4:
-            trigger_sqrl4.configure(state='normal', image=squirell)
+            squirell_sound.play()
+            trigger_sqrl4.configure(
+                state='normal',
+                image=squirell, )
             trigger_sqrl4.place(x=400, y=400)
-            trigger_sqrl4.bind("<Button-1>", lambda event, button=trigger_sqrl4: hit_squirrel(event, button))
             sleep(0.75)
-            trigger_sqrl4.configure(state='disabled')
+            trigger_sqrl4.configure(state='disabled', text='', )
             trigger_sqrl4.place_forget()
         elif hole == 5:
-            trigger_sqrl5.configure(state='normal', image=squirell)
+            squirell_sound.play()
+            trigger_sqrl5.configure(
+                state='normal',
+                image=squirell, )
             trigger_sqrl5.place(x=790, y=400)
-            trigger_sqrl5.bind("<Button-1>", lambda event, button=trigger_sqrl5: hit_squirrel(event, button))
             sleep(0.75)
-            trigger_sqrl5.configure(state='disabled')
+            trigger_sqrl5.configure(state='disabled', text='', )
             trigger_sqrl5.place_forget()
         elif hole == 6:
-            trigger_sqrl6.configure(state='normal', image=squirell)
+            squirell_sound.play()
+            trigger_sqrl6.configure(
+                state='normal',
+                image=squirell, )
             trigger_sqrl6.place(x=210, y=530)
-            trigger_sqrl6.bind("<Button-1>", lambda event, button=trigger_sqrl6: hit_squirrel(event, button))
             sleep(0.75)
-            trigger_sqrl6.configure(state='disabled')
+            trigger_sqrl6.configure(state='disabled', text='', )
             trigger_sqrl6.place_forget()
         elif hole == 7:
-            trigger_sqrl7.configure(state='normal', image=squirell)
+            squirell_sound.play()
+            trigger_sqrl7.configure(
+                state='normal',
+                image=squirell, )
             trigger_sqrl7.place(x=600, y=530)
-            trigger_sqrl7.bind("<Button-1>", lambda event, button=trigger_sqrl7: hit_squirrel(event, button))
             sleep(0.75)
-            trigger_sqrl7.configure(state='disabled')
+            trigger_sqrl7.configure(state='disabled', text='', )
             trigger_sqrl7.place_forget()
         elif hole == 8:
-            trigger_sqrl8.configure(state='normal', image=squirell)
+            squirell_sound.play()
+            trigger_sqrl8.configure(
+                state='normal',
+                image=squirell, )
             trigger_sqrl8.place(x=1000, y=520)
-            trigger_sqrl8.bind("<Button-1>", lambda event, button=trigger_sqrl8: hit_squirrel(event, button))
             sleep(0.75)
-            trigger_sqrl8.configure(state='disabled')
+            trigger_sqrl8.configure(state='disabled', text='', )
             trigger_sqrl8.place_forget()
     county.place_forget()
+    if score > highscore and score != 1000:
+        background_music.stop()
+        new_highscore.play()
+        highscore = score
+        highscore_dekhau.configure(text=f"High Score: {score}")
+        kailesurugarne.place(x=570, y=130)
+        kailesurugarne.configure(text="New High Score!!!")
+        sleep(5)
+        kailesurugarne.place_forget()
+    elif score == 700:
+        victory.play()
+        background_music.stop()
+        highscore = score
+        highscore_dekhau.configure(text=f"High Score: {score}")
+        kailesurugarne.place(x=570, y=130)
+        kailesurugarne.configure(text="You Won!!!", fg_color="black")
+        sleep(15)
+        kailesurugarne.configure(fg_color='white')
+    else:
+        sleep(2)
+    playbtn.place(x=600, y=450)
+    htp.place(x=550, y=520)
 
 
 # when the game starts, this function initiates the countdown... "ready", "set", "whack!!!"
@@ -156,7 +193,10 @@ def how():
     playbtn.place(x=1250, y=700)
 
 
-# MUSIC
+# these are all just the buttons and labels that willl be used throughout the code.. aka variables ig?
+
+
+# MUSIC!!!!
 pygame.mixer.init()
 background_music = pygame.mixer.Sound("background_music.mp3")
 squirell_sound = pygame.mixer.Sound("squirell sound.mp3")
@@ -192,13 +232,13 @@ highscore_dekhau.place(x=960, y=130)
 
 # Countdown !!!
 
-county = customtkinter.CTkLabel(master=Home_screen, font=("PokemonGB", 30), text=f"Squirrels Left:{countdown}")
+county = customtkinter.CTkLabel(master=Home_screen, font=("PokemonGB", 30), text=f"Squirells Left:{countdown}")
 
-
-squirell = customtkinter.CTkImage(Image.open("squirrel.png"), size=(170, 160))
+# rat image
+squirell = customtkinter.CTkImage(Image.open("squirrel.png"), size=(160, 160))
 howpic = customtkinter.CTkImage(Image.open("how.png"), size=(1000, 650))
 
-
+# rat image when it is clicked aka. hit
 bonked = customtkinter.CTkImage(Image.open("squirrel_on_mud.png"), size=(170, 160))
 
 # the "ready!" "set!" "go!" ko placeholder
@@ -250,101 +290,101 @@ menu = customtkinter.CTkLabel(master=Home_screen,
                               image=howpic,
                               text=''
                               )
-
-
+# menu.place(relx =0.5,rely =0.5, anchor =CENTER)
+# squirell1
 trigger_sqrl1 = customtkinter.CTkButton(master=Home_screen,
-                                       command=onwhack,
-                                       text='',
-                                       state='disabled',
-                                       fg_color='#21c65a',
-                                       height=10,
-                                       width=10,
-                                       corner_radius=0,
-                                       hover=False,
-                                       )
+                                        command=onwhack,
+                                        text='',
+                                        state='disabled',
+                                        fg_color='#21c65a',
+                                        height=10,
+                                        width=10,
+                                        corner_radius=0,
+                                        hover=False,
+                                        )
 
 # squirell2
 trigger_sqrl2 = customtkinter.CTkButton(master=Home_screen,
-                                       command=onwhack,
-                                       text='',
-                                       state='disabled',
-                                       fg_color='#21c65a',
-                                       height=10,
-                                       width=10,
-                                       corner_radius=0,
-                                       hover=False,
-                                       )
+                                        command=onwhack,
+                                        text='',
+                                        state='disabled',
+                                        fg_color='#21c65a',
+                                        height=10,
+                                        width=10,
+                                        corner_radius=0,
+                                        hover=False,
+                                        )
 
 # squirell3
 trigger_sqrl3 = customtkinter.CTkButton(master=Home_screen,
-                                       command=onwhack,
-                                       text='',
-                                       state='disabled',
-                                       fg_color='#21c65a',
-                                       height=10,
-                                       width=10,
-                                       corner_radius=0,
-                                       hover=False,
-                                       )
+                                        command=onwhack,
+                                        text='',
+                                        state='disabled',
+                                        fg_color='#21c65a',
+                                        height=10,
+                                        width=10,
+                                        corner_radius=0,
+                                        hover=False,
+                                        )
 
 # squirell4
 trigger_sqrl4 = customtkinter.CTkButton(master=Home_screen,
-                                       command=onwhack,
-                                       text='',
-                                       state='disabled',
-                                       fg_color='#21c65a',
-                                       height=10,
-                                       width=10,
-                                       corner_radius=0,
-                                       hover=False,
-                                       )
+                                        command=onwhack,
+                                        text='',
+                                        state='disabled',
+                                        fg_color='#21c65a',
+                                        height=10,
+                                        width=10,
+                                        corner_radius=0,
+                                        hover=False,
+                                        )
 
 # squirell5
 trigger_sqrl5 = customtkinter.CTkButton(master=Home_screen,
-                                       command=onwhack,
-                                       text='',
-                                       state='disabled',
-                                       fg_color='#21c65a',
-                                       height=10,
-                                       width=10,
-                                       corner_radius=0,
-                                       hover=False,
-                                       )
+                                        command=onwhack,
+                                        text='',
+                                        state='disabled',
+                                        fg_color='#21c65a',
+                                        height=10,
+                                        width=10,
+                                        corner_radius=0,
+                                        hover=False,
+                                        )
 
 # squirell6
 trigger_sqrl6 = customtkinter.CTkButton(master=Home_screen,
-                                       command=onwhack,
-                                       text='',
-                                       state='disabled',
-                                       fg_color='#21c65a',
-                                       height=10,
-                                       width=10,
-                                       corner_radius=0,
-                                       hover=False,
-                                       )
+                                        command=onwhack,
+                                        text='',
+                                        state='disabled',
+                                        fg_color='#21c65a',
+                                        height=10,
+                                        width=10,
+                                        corner_radius=0,
+                                        hover=False,
+                                        )
 
 # squirell7
 trigger_sqrl7 = customtkinter.CTkButton(master=Home_screen,
-                                       command=onwhack,
-                                       text='',
-                                       state='disabled',
-                                       fg_color='#21c65a',
-                                       height=10,
-                                       width=10,
-                                       corner_radius=0,
-                                       hover=False,
-                                       )
+                                        command=onwhack,
+                                        text='',
+                                        state='disabled',
+                                        fg_color='#21c65a',
+                                        height=10,
+                                        width=10,
+                                        corner_radius=0,
+                                        hover=False,
+                                        )
 
 # squirell8
 trigger_sqrl8 = customtkinter.CTkButton(master=Home_screen,
-                                       command=onwhack,
-                                       text='',
-                                       state='disabled',
-                                       fg_color='#21c65a',
-                                       height=10,
-                                       width=10,
-                                       corner_radius=0,
-                                       hover=False,
-                                       )
+                                        command=onwhack,
+                                        text='',
+                                        state='disabled',
+                                        fg_color='#21c65a',
+                                        height=10,
+                                        width=10,
+                                        corner_radius=0,
+                                        hover=False,
+                                        )
 
 app.mainloop()
